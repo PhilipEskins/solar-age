e9f2c7895062d111995ac91a8f60d4d5573993dd
Philip Eskins <philipeskins@gmail.com>
2019-03-15 16:56:00 -0700

    make new function to print out the results so they don't show up in Karma and fixed one failing spec

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss';
 import './sass/styles.scss';
-import { earthAge } from './../src/solar-age.js';
+import { printResults } from './../src/solar-age.js';
 
 $(document).ready(function() {
   $("#getBirthday").submit(function(event) {
     event.preventDefault();
     const dayOfBirth = $("#dayOfBirth").val();
-    earthAge(dayOfBirth);
+    printResults(dayOfBirth);
   })
 })
