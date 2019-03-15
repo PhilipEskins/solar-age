fb5619f2b3d80c4e5e67c14c6c24386773d4f821
Philip Eskins <philipeskins@gmail.com>
2019-03-15 15:25:29 -0700

    add life expectancy for venus, mars, jupiter and test for each function

--- spec/tester-spec.js
+++ spec/tester-spec.js
@@ -1,4 +1,4 @@
-import { earthAge, Person } from './../src/solar-age.js';
+import { earthAge, liveLong, Person } from './../src/solar-age.js';
 
 describe('Solar Age', function(){
   // beforeEach(function() {
@@ -10,33 +10,47 @@ describe('Solar Age', function(){
   });
 
   it('should return your age on Mercury', function() {
-    const reusablePerson = new Person(36);
+    const reusablePerson = new Person(36, 0);
     expect(reusablePerson.mercAge()).toEqual(150);
   });
 
   it('should return your age on Venus', function() {
-    const reusablePerson = new Person(36);
+    const reusablePerson = new Person(36, 0);
     expect(reusablePerson.venusAge()).toEqual(58);
   });
 
   it('should return your age on Mars', function() {
-    const reusablePerson = new Person(36);
+    const reusablePerson = new Person(36, 0);
     expect(reusablePerson.marsAge()).toEqual(19);
   });
 
   it('should return your age on Jupiter', function() {
-    const reusablePerson = new Person(36);
+    const reusablePerson = new Person(36, 0);
     expect(reusablePerson.jupAge()).toEqual(3);
   });
 
   it('should return your life expectancy on Earth', function() {
-    const reusablePerson = new Person(36);
-    expect(reusablePerson.liveLong()).toEqual(42);
+    expect(liveLong(36)).toEqual(42);
   });
 
   it('should return your life expectancy on Mercury', function() {
-    const reusablePerson = new Person(36);
+    const reusablePerson = new Person(0, 42);
     expect(reusablePerson.mercLife()).toEqual(175);
+  });
+
+  it('should return your life expectancy on Venus', function() {
+    const reusablePerson = new Person(0, 42);
+    expect(reusablePerson.venusLife()).toEqual(67);
+  });
+
+  it('should return your life expectancy on Mars', function() {
+    const reusablePerson = new Person(0, 42);
+    expect(reusablePerson.marsLife()).toEqual(22);
+  });
+
+  it('should return your life expectancy on Jupiter', function() {
+    const reusablePerson = new Person(0, 42);
+    expect(reusablePerson.jupLife()).toEqual(3);
   });
 
 });
--- src/solar-age.js
+++ src/solar-age.js
@@ -1,8 +1,8 @@
 class Person {
-  constructor(age) {
+  constructor(age, life) {
     const self=this;
     this.age = age;
-    this.life = 0;
+    this.life = life;
   }
   mercAge() {
     return Math.floor(this.age / 0.24);
@@ -16,15 +16,17 @@ class Person {
   jupAge() {
     return Math.floor(this.age / 11.86);
   }
-  liveLong() {
-    const lifeExpect = 78;
-    const remain = lifeExpect - this.age;
-    this.life = remain;
-    return this.life;
-  }
   mercLife() {
-    self.liveLong();
     return Math.floor(this.life / 0.24);
+  }
+  venusLife() {
+    return Math.floor(this.life / 0.62);
+  }
+  marsLife() {
+    return Math.floor(this.life / 1.88);
+  }
+  jupLife() {
+    return Math.floor(this.life / 11.86);
   }
 }
 
@@ -36,16 +38,26 @@ function earthAge(dateInput) {
   if (month < 0 || (month === 0 && current.getDate() < birthday.getDate())) {
     age--;
   }
-  const test = new Person(age);
+  const life = liveLong(age);
+  const test = new Person(age, life);
   console.log(test.mercAge());
   console.log(test.venusAge());
   console.log(test.marsAge());
   console.log(test.jupAge());
-  console.log(test.liveLong());
+  console.log(life);
   console.log(test.mercLife());
+  console.log(test.venusLife());
+  console.log(test.marsLife());
+  console.log(test.jupLife());
   return age;
+}
+
+function liveLong(age) {
+  const lifeExpect = 78;
+  const life = lifeExpect-age;
+  return life;
 }
 
 console.log(earthAge("10/04/1982"));
 
-export {earthAge, Person};
+export {earthAge, liveLong, Person};
